b0da1809c0bc8afeaca53624963fc30f64591da9
mjezior <mateusz.jeziorek@o2.pl>
2018-11-03 15:19:03 +0100

    fix: corrected install dir path

--- src/install.js
+++ src/install.js
@@ -5,7 +5,7 @@
   const copyDirectory = require('./utils/copy-directory.util');
 
   const sourceDir = 'generate-files';
-  const destDir = `${__dirname + path.sep}.fsg`;
+  const destDir = `${process.env.INIT_CWD + path.sep}.fsg`;
 
   copyDirectory(sourceDir, destDir).then(() => {
     console.log('installed local .fsg directory'); // eslint-disable-line no-console
