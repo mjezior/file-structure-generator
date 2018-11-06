e8273c15b66d131cb388a12a9fe759f90d231e10
mjezior <mateusz.jeziorek@o2.pl>
2018-11-06 01:15:38 +0100

    update: made logger util more universal by allowing unlimited styling parameters

--- src/utils/logger.util.js
+++ src/utils/logger.util.js
@@ -1,12 +1,21 @@
 const chalk = require('chalk');
 
 const _isArray = require('lodash/isArray');
+const _reduce = require('lodash/reduce');
 
 module.exports = logger = {
   log: (...messages) => {
     messages = messages.map((message) => {
       if (_isArray(message)) {
-        return message[1] ? chalk[message[1]](message[0]) : message[0];
+        if (message.length > 1) {
+          const messageText = message.shift();
+          const chalkChain = _reduce(message, (chain, method) => (
+            chain[method]
+          ), chalk);
+          return chalkChain(messageText);
+        } else {
+          return message[0];
+        }
       } else {
         return message;
       }
