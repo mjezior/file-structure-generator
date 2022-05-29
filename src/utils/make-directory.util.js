b477f847aff3e945f30c7f57c6fb4dfeccc3bedf
jsfix.live analysis <info@coana.tech>
2022-05-29 08:46:39 +0200

    JSFIX patches

--- src/utils/make-directory.util.js
+++ src/utils/make-directory.util.js
@@ -3,6 +3,9 @@ const fs = require('fs-extra');
 const logger = require('./logger.util');
 
 module.exports = makeDirectory = (dirName) => (
+  /* TODO: JSFIX could not patch the breaking change:
+  Creating a directory with fs-extra no longer returns the path 
+  Suggested fix: The returned promise no longer includes the path of the new directory */
   fs.mkdirs(dirName, null)
     .catch(err => logger.log([`Error: ${err}`, 'bgRed']))
 );
